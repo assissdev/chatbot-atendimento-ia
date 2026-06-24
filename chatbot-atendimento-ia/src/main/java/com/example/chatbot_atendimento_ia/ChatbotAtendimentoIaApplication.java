bd25a322524b03501630e24ae8559b7279d96ce4
AssisDev <matheusdonaal@gmail.com>
2026-06-23 23:33:33 -0300

    chore: setup inicial do projeto Spring Boot

---
+++ chatbot-atendimento-ia/src/main/java/com/example/chatbot_atendimento_ia/ChatbotAtendimentoIaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatbot_atendimento_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotAtendimentoIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotAtendimentoIaApplication.class, args);
+	}
+
+}
---
+++ chatbot-atendimento-ia/src/test/java/com/example/chatbot_atendimento_ia/ChatbotAtendimentoIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatbot_atendimento_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotAtendimentoIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
